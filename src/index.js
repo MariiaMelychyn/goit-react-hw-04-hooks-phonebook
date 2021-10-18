99de28db12aa94f792bd89b02f4317b8aba0f77c
MariiaMelychyn <lv080191mmi75@gmail.com>
2021-10-18 20:04:02 +0300

    src

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import 'modern-normalize/modern-normalize.css';
-import './App.module.scss';
-
+import App from './App';
+import 'index.css';
 
 ReactDOM.render(
   <React.StrictMode>
